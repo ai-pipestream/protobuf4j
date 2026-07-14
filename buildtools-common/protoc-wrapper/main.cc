10c986ebdc62ce1ba436b1fe0926930282b476d6
Kristian Rickert <krickert@gmail.com>
2026-07-14 09:26:16 -0400

    Add the Ruby generator to the protoc wrapper
    libprotoc's ruby::Generator exposed through the wrapper dispatch as
    'ruby' and as NativePlugin.RUBY.

--- buildtools-common/protoc-wrapper/main.cc
+++ buildtools-common/protoc-wrapper/main.cc
@@ -7,7 +7,7 @@
 int main(int argc, char** argv) {
   if (argc < 2) {
     std::cerr << "Usage: " << argv[0]
-              << " <java | kotlin | grpc-java | python | cpp | csharp>\n";
+              << " <java | kotlin | grpc-java | python | cpp | csharp | ruby>\n";
     return 1;
   }
 
@@ -34,6 +34,9 @@ int main(int argc, char** argv) {
   }
   if (option == "csharp") {
     return protoc_wrapper::RunCSharpGenerator(argc, argv, 2);
+  }
+  if (option == "ruby") {
+    return protoc_wrapper::RunRubyGenerator(argc, argv, 2);
   }
 
   std::cerr << "Unknown option: " << option << "\n";
