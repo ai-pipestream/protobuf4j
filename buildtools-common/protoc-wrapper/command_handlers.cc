f8e31713ffeef19ada01410043d1cc9a2ad604b1
Kristian Rickert <krickert@gmail.com>
2026-07-14 09:25:56 -0400

    Add the C# generator to the protoc wrapper
    libprotoc's csharp::Generator exposed through the wrapper dispatch as
    'csharp' and as NativePlugin.CSHARP.

--- buildtools-common/protoc-wrapper/command_handlers.cc
+++ buildtools-common/protoc-wrapper/command_handlers.cc
@@ -14,6 +14,7 @@
 #endif
 #include <google/protobuf/compiler/parser.h>
 #include <google/protobuf/compiler/python/generator.h>
+#include <google/protobuf/compiler/csharp/csharp_generator.h>
 #include <google/protobuf/compiler/cpp/generator.h>
 #include <google/protobuf/compiler/plugin.h>
 #include <google/protobuf/descriptor.h>
@@ -65,6 +66,11 @@ int ValidateProtoFileReadable(const std::string& file) {
 int RunCppGenerator(int argc, char** argv, int start_index) {
   google::protobuf::compiler::cpp::CppGenerator generator;
   return RunPluginCommand("protoc-gen-cpp", &generator, argc, argv, start_index);
+}
+
+int RunCSharpGenerator(int argc, char** argv, int start_index) {
+  google::protobuf::compiler::csharp::Generator generator;
+  return RunPluginCommand("protoc-gen-csharp", &generator, argc, argv, start_index);
 }
 
 int RunPythonGenerator(int argc, char** argv, int start_index) {
--- buildtools-common/protoc-wrapper/main.cc
+++ buildtools-common/protoc-wrapper/main.cc
@@ -7,7 +7,7 @@
 int main(int argc, char** argv) {
   if (argc < 2) {
     std::cerr << "Usage: " << argv[0]
-              << " <java | kotlin | grpc-java | python | cpp>\n";
+              << " <java | kotlin | grpc-java | python | cpp | csharp>\n";
     return 1;
   }
 
@@ -31,6 +31,9 @@ int main(int argc, char** argv) {
   }
   if (option == "cpp") {
     return protoc_wrapper::RunCppGenerator(argc, argv, 2);
+  }
+  if (option == "csharp") {
+    return protoc_wrapper::RunCSharpGenerator(argc, argv, 2);
   }
 
   std::cerr << "Unknown option: " << option << "\n";
