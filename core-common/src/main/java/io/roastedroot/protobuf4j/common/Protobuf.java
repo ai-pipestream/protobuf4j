f92ead791bf8e68826836fcfe48c1d2f5865970d
Kristian Rickert <krickert@gmail.com>
2026-07-14 09:26:16 -0400

    Add the Ruby generator to the protoc wrapper
    libprotoc's ruby::Generator exposed through the wrapper dispatch as
    'ruby' and as NativePlugin.RUBY.

--- core-common/src/main/java/io/roastedroot/protobuf4j/common/Protobuf.java
+++ core-common/src/main/java/io/roastedroot/protobuf4j/common/Protobuf.java
@@ -69,7 +69,8 @@ public final class Protobuf {
         GRPC_JAVA("grpc-java"),
         PYTHON("python"),
         CPP("cpp"),
-        CSHARP("csharp");
+        CSHARP("csharp"),
+        RUBY("ruby");
 
         private final String value;
 
